45b27b2c25747ba5d09127dc843f7f5e117ca610
zackhitch <zackhitch@users.noreply.github.com>
2018-03-19 13:58:56 -0700

    completed increment and decrement, not sure how it works though - Zack Hitchcock

--- immutable.js
+++ immutable.js
@@ -15,6 +15,16 @@
   Be sure to console.log() both references to ensure that they are pointing to different objects.
 */
 
+const person = {
+  name: 'Dwight Schrute',
+  age: 31,
+  hometown: 'Scranton, PA'
+};
+
+const newPerson = Object.assign({}, person);
+newPerson.name = 'Jim Halpert';
+newPerson.age = 25;
+
 console.log(person);
 console.log(newPerson);
 
@@ -23,6 +33,25 @@ console.log(newPerson);
   Create a separate array of `superVillains`
   Concatenate the two of them together using `.concat()`
 */
+
+const superHeroes = [
+  'Ironman',
+  'Thor',
+  'The Hulk',
+  'Spiderman',
+  'Doctor Strange',
+  'Captain America'
+];
+const superVillains = [
+  'Loki',
+  'Thanos',
+  'Ultron',
+  'Juggernaut',
+  'Magneto',
+  'Bucky Barnes'
+];
+
+const combinedSupers = superHeroes.concat(superVillains);
 
 console.log(superHeroes);
 console.log(superVillains);
@@ -35,6 +64,15 @@ console.log(combinedSupers);
   Create a new object `studentAverage` using Object.assign() from the given original object.
   The new object should filter out any scores that are < 34 Array.filter should be used here.
 */
+
+const studentScores = {
+  name: 'Timmy',
+  age: 22,
+  scores: [20, 50, 48, 44, 32, 50]
+};
+
+const studentAverage = Object.assign({}, studentScores);
+studentAverage.scores = studentScores.scores.filter(score => score < 34);
 
 console.log(studentScores);
 console.log(studentAverage);
